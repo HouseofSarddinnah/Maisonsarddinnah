4c4b83c1605cf37398c99ef3b46af4311ed38326
Maisonsarddinnah <thehouseofsarddinnah@yahoo.com>
2026-08-25 17:01:23 -0400

    chore: standardize Travelpayouts Drive script sitewide

--- worker.js
+++ worker.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.hostname === "www.maisonsarddinnah.com") {
+      url.hostname = "maisonsarddinnah.com";
+      return Response.redirect(url, 301);
+    }
+
     return env.ASSETS.fetch(request);
   }
 };
